84a4686222cfd0242758a7802b90cf2122fbde88
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-08-26 06:15:39 +0530

    Fix variable namings

--- index.js
+++ index.js
@@ -5,34 +5,34 @@ const chalk = require("chalk");
 const isObject = require('validate.io-object');
 const isString = require('validate.io-string-primitive');
 
-let opts = {};
+let options = {};
 
-function validate(options) {
-  if (!isObject(options)) {
-    return new TypeError('invalid input argument. Options argument must be an object. Value: `' + options + '`.');
+function validate(_options) {
+  if (!isObject(_options)) {
+    return new TypeError('invalid input argument. Options argument must be an object. Value: `' + _options + '`.');
   }
-  if (options.hasOwnProperty('username') || options.hasOwnProperty('u')) {
-    opts.username = options.username || options.u;
-    if (!isString(opts.username)) {
-      return new TypeError('invalid option. Username must be a string primitive. Option: `' + opts.username + '`.');
+  if (_options.hasOwnProperty('username') || _options.hasOwnProperty('u')) {
+    options.username = _options.username || _options.u;
+    if (!isString(options.username)) {
+      return new TypeError('invalid option. Username must be a string primitive. Option: `' + options.username + '`.');
     }
   }
-  if (options.hasOwnProperty('token') || options.hasOwnProperty('t')) {
-    opts.token = options.token || options.t;
-    if (!isString(opts.token)) {
-      return new TypeError('invalid option. Token must be a string primitive. Option: `' + opts.token + '`.');
+  if (_options.hasOwnProperty('token') || _options.hasOwnProperty('t')) {
+    options.token = _options.token || _options.t;
+    if (!isString(options.token)) {
+      return new TypeError('invalid option. Token must be a string primitive. Option: `' + options.token + '`.');
     }
   }
-  if (options.hasOwnProperty('repo') || options.hasOwnProperty('r')) {
-    opts.repo = options.repo || options.r;
-    if (!isString(opts.repo)) {
-      return new TypeError('invalid option. Repo name must be a string primitive. Option: `' + opts.repo + '`.');
+  if (_options.hasOwnProperty('repo') || _options.hasOwnProperty('r')) {
+    options.repo = _options.repo || _options.r;
+    if (!isString(options.repo)) {
+      return new TypeError('invalid option. Repo name must be a string primitive. Option: `' + options.repo + '`.');
     }
   }
-  if (options.hasOwnProperty('message') || options.hasOwnProperty('m')) {
-    opts.message = options.message || options.m;
-    if (!isString(opts.message)) {
-      return new TypeError('invalid option. Commit message must be a string primitive. Option: `' + opts.message + '`.');
+  if (_options.hasOwnProperty('message') || _options.hasOwnProperty('m')) {
+    options.message = _options.message || _options.m;
+    if (!isString(options.message)) {
+      return new TypeError('invalid option. Commit message must be a string primitive. Option: `' + options.message + '`.');
     }
   }
   return null;
@@ -51,13 +51,28 @@ module.exports = (_options) => {
     return;
   }
 
-  console.log(opts);
+  console.log(options);
 
-  const {u, username} = _options;
+  const { username } = options;
+  const url = `users/${username}/starred`;
 
-  const url = `users/${u || username}/starred`;
-  return (async () => {
-      const response = await ghGot(url, _options);
+  (async () => {
+    let response = [];
+
+    try {
+      response = await ghGot(url, _options);
+    } catch(err) {
+      console.warn(
+        chalk.bold.green(`Error while fetching data!`)
+      );
+      return;
+    }
+
+    if (!response.length) {
+      console.warn(
+        chalk.bold.green(`Fetch complete!`)
+      );
+    }
 
       // console.log(response);
 
