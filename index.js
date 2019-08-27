cfd3589e4e85436cfa85f37969e1f1806af19bad
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-08-27 20:50:35 +0530

    add username as a required field & other minor tweaks

--- index.js
+++ index.js
@@ -22,9 +22,7 @@ const validate = _options => {
 	) {
 		options.username = _options.username || _options.u;
 		if (!isString(options.username)) {
-			return new TypeError(
-				`invalid option. Username must be a string primitive. Option: \`${options.username}\`.`
-			);
+			return new TypeError(`invalid option. Username must be a string primitive.`);
 		}
 	}
 	if (
@@ -33,13 +31,13 @@ const validate = _options => {
 	) {
 		options.token = _options.token || _options.t;
 		if (!isString(options.token)) {
-			return new TypeError(`invalid option. Token must be a string primitive. Option: \`${options.token}\`.`);
+			return new TypeError(`invalid option. Token must be a string primitive.`);
 		}
 	}
 	if (Object.prototype.hasOwnProperty.call(_options, 'repo') || Object.prototype.hasOwnProperty.call(_options, 'r')) {
 		options.repo = _options.repo || _options.r;
 		if (!isString(options.repo)) {
-			return new TypeError(`invalid option. Repo name must be a string primitive. Option: \`${options.repo}\`.`);
+			return new TypeError(`invalid option. Repo name must be a string primitive.`);
 		}
 	}
 	if (
@@ -48,17 +46,13 @@ const validate = _options => {
 	) {
 		options.message = _options.message || _options.m;
 		if (!isString(options.message)) {
-			return new TypeError(
-				`invalid option. Commit message must be a string primitive. Option: \`${options.message}\`.`
-			);
+			return new TypeError(`invalid option. Commit message must be a string primitive.`);
 		}
 	}
 	if (Object.prototype.hasOwnProperty.call(_options, 'sort') || Object.prototype.hasOwnProperty.call(_options, 's')) {
 		options.sort = _options.sort || _options.s;
 		if (!isBoolean(options.sort)) {
-			return new TypeError(
-				`invalid option. Sort option must be a boolean primitive. Option: \`${options.sort}\`.`
-			);
+			return new TypeError(`invalid option. Sort option must be a boolean primitive.`);
 		}
 	}
 	if (
@@ -67,17 +61,13 @@ const validate = _options => {
 	) {
 		options.version = _options.version || _options.v;
 		if (!isBoolean(options.version)) {
-			return new TypeError(
-				`invalid option. Version option must be a boolean primitive. Option: \`${options.version}\`.`
-			);
+			return new TypeError(`invalid option. Version option must be a boolean primitive.`);
 		}
 	}
 	if (Object.prototype.hasOwnProperty.call(_options, 'help') || Object.prototype.hasOwnProperty.call(_options, 'h')) {
 		options.help = _options.help || _options.h;
 		if (!isBoolean(options.help)) {
-			return new TypeError(
-				`invalid option. Help option must be a boolean primitive. Option: \`${options.help}\`.`
-			);
+			return new TypeError(`invalid option. Help option must be a boolean primitive.`);
 		}
 	}
 	return null;
@@ -149,6 +139,7 @@ const writeReadmeContent = async readmeContent => {
 		spinner.fail('README creation fail');
 		flashError(err);
 	}
+	spinner.stop();
 };
 
 module.exports = _options => {
@@ -156,6 +147,23 @@ module.exports = _options => {
 
 	if (err) {
 		flashError(err);
+		return;
+	}
+
+	const { username, sort, help, version } = options;
+
+	if (help) {
+		// ToDo: Show the commands
+		return;
+	}
+
+	if (version) {
+		// ToDo: Show the version info
+		return;
+	}
+
+	if (!username) {
+		flashError('Error! username is a required field.');
 		return;
 	}
 
@@ -168,22 +176,23 @@ module.exports = _options => {
 		};
 	}
 
-	const { username, sort } = options;
 	const url = `users/${username}/starred`;
 
 	(async () => {
 		let response = {};
 		const stargazed = {};
+		const spinner = ora('Fetching repositories').start();
+
 		try {
 			response = await ghGot(url, _options);
+			spinner.succeed('Data fetch successfully completed!');
 		} catch (err) {
-			console.warn(chalk.bold.green(`Error while fetching data!`));
+			spinner.fail('Error while fetching data!');
+			flashError(err);
 			return;
 		}
 
-		if (!Object.keys(response).length) {
-			console.warn(chalk.bold.green(`Fetch complete!`));
-		}
+		spinner.stop();
 
 		const { body } = response;
 
