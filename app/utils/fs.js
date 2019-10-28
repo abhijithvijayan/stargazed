e01b2667879366211cd156cb6dad60616d01d9b1
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-10-28 23:50:57 +0530

    refactor: add util function to perform async read/write

---
+++ app/utils/fs.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const { promisify } = require('util');
+
+const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
+
+module.exports = {
+	readFileAsync,
+	writeFileAsync,
+};
