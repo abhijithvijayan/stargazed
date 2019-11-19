dfa67607228285ae0dcbe988ecacc8f4afe79fae
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-11-19 14:25:30 +0530

    fix: remove deprecated chalk.default() function

--- mock/contentInput.js
+++ mock/contentInput.js
@@ -3,7 +3,10 @@ export const inputContent = {
 	username: 'Jean-Luc-Picard',
 	count: Object.keys([1, 2, 3, 4]).length,
 	stargazed: {
-		Klingon: [[1, 'URL', 'description', 'author', 1000], [2, 'URL', 'description', 'author', 2000]],
+		Klingon: [
+			[1, 'URL', 'description', 'author', 1000],
+			[2, 'URL', 'description', 'author', 2000],
+		],
 		English: [[2, 'URL', 'description', 'author', 2000]],
 		Vulcan: [[3, 'URL', 'description', 'author', 3000]],
 		JavaScript: [[4, 'URL', 'description', 'author', 4000]],
